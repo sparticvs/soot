75d7834febfa61a93db33b62ddc1290477f0e729
Charles Timko <sparticvs@popebp.com>
2022-01-05 14:00:02 -0500

    Initial version that probably works

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # soot
 Upstream Tracker updater for RPM SPEC files.
+
+This is intended to be used where the RPM SPEC file is in a different repo from
+the upstream (essentially if it was in the same repo, the workflow is
+different).
+
+## Requirements
+  - Use of Python 3.8 or later
+  - rpmdev-bumpspec installed (provided by rpmdevtools)
+
+```
+$ python -m venv .venv
+$ source .venv/bin/activate
+$ pip -r requirements.txt
+```
+
+## Usage
+
+Update the configuration file to use your details. Recommend installing this to
+a crontab or similar.
+
+```
+$ python main.py
+```
---
+++ main.py
@@ -0,0 +1,90 @@
+import subprocess
+import argparse
+import configparser
+from github import Github
+from pyrpm.spec import Spec
+from pygit2 import clone_repository, discover_repository, Repository
+
+parser = argparse.ArgumentParser(
+    description='Monitor Upstream for Changes to Trigger RPM SPEC Changes')
+parser.add_argument('--config', nargs='?', default='soot.cfg', type=str,
+    help='Change config file from default soot.cfg.')
+args = parser.parse_args()
+
+config = configparser.ConfigParser()
+config.read(args.config)
+
+GITHUB_UPSTREAM = config['upstream']['github']
+GITHUB_SPEC_REPO = config['spec']['github']
+GITHUB_ACCESS_TOKEN = config['github']['access_token']
+UPSTREAM_SPEC_REPO = config['spec']['clone_url']
+UPSTREAM_SPEC_BRANCH = config['spec']['branch']
+UPSTREAM_SPEC_REMOTE = config['spec']['remote']
+SPEC_FILE = config['spec']['file']
+SRPM_TMP_REPO = config['default']['tmp_repo']
+MAINTAINER_STR = config['default']['maintainer']
+
+def strip_version(vers):
+    if vers[0] == "v":
+        return vers[1:]
+    return vers
+
+spec_repo = None
+spec_origin = None
+repo_path = discover_repository(SRPM_TMP_REPO)
+if repo_path is None:
+    try:
+        spec_repo = clone_repository(UPSTREAM_SPEC_REPO, SRPM_TMP_REPO,
+            checkout_branch=UPSTREAM_SPEC_BRANCH)
+        spec_origin = spec_repo.remotes[UPSTREAM_SPEC_REMOTE]
+        spec_origin.connect()
+    except Exception as e:
+        print("Unable to clone repository")
+        print(e)
+else:
+    spec_repo = Repository(repo_path)
+    (spec_commit, spec_ref) = spec_repo.resolve_refish(UPSTREAM_SPEC_BRANCH)
+    spec_repo.checkout(spec_ref)
+    # Fetch latest head
+    spec_origin = spec_repo.remotes[UPSTREAM_SPEC_REMOTE]
+    spec_origin.connect()
+    r = spec_origin.fetch()
+    print(f'Fetch returned {r.total_deltas} deltas and {r.total_objects} objects')
+
+if spec_repo is None or spec_origin is None:
+    raise Exception('Something happened that wasn\'t caught!')
+
+g = Github(GITHUB_ACCESS_TOKEN)
+up_repo = g.get_repo(GITHUB_UPSTREAM)
+latest_rel = up_repo.get_latest_release()
+latest_ver = strip_version(latest_rel.tag_name)
+
+spec = Spec.from_file(f'{SRPM_TMP_REPO}/{SPEC_FILE}')
+if spec.version != latest_ver:
+    print(f'Updating SPEC from {spec.version} to {latest_ver}')
+    new_branch = spec_repo.branches.local.create(f'update/v{latest_ver}')
+    spec_repo.checkout(f'update/v{latest_ver}')
+    rc = subprocess.run(["rpmdev-bumpspec", "-n", latest_ver,
+                    f'--comment="Updating to v{latest_ver}"',
+                    f'--userstring="{MAINTAINER_STR}"'])
+    print(f'Update bumpspec return code is {rc.returncode}')
+    # Update the working index
+    index = spec_repo.index
+    index.add(SPEC_FILE)
+    index.write()
+    # Push branch to remote
+    (commit, ref) = spec_repo.reslove_refish(f'update/v{latest_ver}')
+    r = spec_origin.push(ref)
+    print(f'Push returned {r}')
+    ## And open a Merge Request
+    github_spec = g.get_repo(GITHUB_SPEC)
+    body = f'''
+    # Summary
+    Bump SPEC Version to latest release ({latest_rel}) from {GITHUB_UPSTREAM}
+    '''
+    github_spec.create_pull(title=f'Bump SPEC Version to {latest_rel}',
+            body=body, head=f'update/v{latest_ver}',
+            base=UPSTREAM_SPEC_BRANCH)
+else:
+    print(f'SPEC is at {latest_ver} already')
+
