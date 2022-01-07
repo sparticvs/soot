742d01aac5b697d5e6a38a296af01dcff4d8bedb
Charles Timko <sparticvs@popebp.com>
2022-01-07 08:41:54 -0500

    Add support for using SSH Keypairs

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import argparse
 import configparser
 from github import Github
 from pyrpm.spec import Spec
-from pygit2 import clone_repository, discover_repository, Repository
+from pygit2 import (
+        clone_repository, discover_repository, Repository, Keypair,
+        RemoteCallbacks)
 
 parser = argparse.ArgumentParser(
     description='Monitor Upstream for Changes to Trigger RPM SPEC Changes')
@@ -31,13 +33,22 @@ def strip_version(vers):
 
 spec_repo = None
 spec_origin = None
-repo_path = discover_repository(SRPM_TMP_REPO)
+repo_path = None
+try: 
+    repo_path = discover_repository(SRPM_TMP_REPO)
+except KeyError:
+    pass
+keypair = Keypair('git', '/home/ctimko/.ssh/id_ed25519.pub',
+        '/home/ctimko/.ssh/id_ed25519', '')
+#keypair = Keypair('git', 'id_ed25519.pub',
+#        'id_ed25519', '')
+callbacks = RemoteCallbacks(credentials=keypair)
 if repo_path is None:
     try:
         spec_repo = clone_repository(UPSTREAM_SPEC_REPO, SRPM_TMP_REPO,
-            checkout_branch=UPSTREAM_SPEC_BRANCH)
+            checkout_branch=UPSTREAM_SPEC_BRANCH, callbacks=callbacks)
         spec_origin = spec_repo.remotes[UPSTREAM_SPEC_REMOTE]
-        spec_origin.connect()
+        spec_origin.connect(callbacks=callbacks)
     except Exception as e:
         print("Unable to clone repository")
         print(e)
@@ -62,19 +73,24 @@ latest_ver = strip_version(latest_rel.tag_name)
 spec = Spec.from_file(f'{SRPM_TMP_REPO}/{SPEC_FILE}')
 if spec.version != latest_ver:
     print(f'Updating SPEC from {spec.version} to {latest_ver}')
-    new_branch = spec_repo.branches.local.create(f'update/v{latest_ver}')
-    spec_repo.checkout(f'update/v{latest_ver}')
+    (spec_commit, spec_ref) = spec_repo.resolve_refish(UPSTREAM_SPEC_BRANCH)
+    if spec_repo.branches.get(f'update/v{latest_ver}') is None:
+        new_branch = spec_repo.branches.local.create(f'update/v{latest_ver}',
+                spec_commit)
+    (latest_commit, latest_ref) = spec_repo.resolve_refish(f'update/v{latest_ver}')
+    spec_repo.checkout(latest_ref)
     rc = subprocess.run(["rpmdev-bumpspec", "-n", latest_ver,
                     f'--comment="Updating to v{latest_ver}"',
-                    f'--userstring="{MAINTAINER_STR}"'])
+                    f'--userstring="{MAINTAINER_STR}"',
+                    f'{SRPM_TMP_REPO}/{SPEC_FILE}'])
     print(f'Update bumpspec return code is {rc.returncode}')
     # Update the working index
     index = spec_repo.index
     index.add(SPEC_FILE)
     index.write()
     # Push branch to remote
-    (commit, ref) = spec_repo.reslove_refish(f'update/v{latest_ver}')
-    r = spec_origin.push(ref)
+    (commit, ref) = spec_repo.resolve_refish(f'update/v{latest_ver}')
+    r = spec_origin.push([f'update/v{latest_ver}'], callbacks=callbacks)
     print(f'Push returned {r}')
     ## And open a Merge Request
     github_spec = g.get_repo(GITHUB_SPEC)
